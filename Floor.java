37c5270601a0c92e9321f39642bfe8eaec1ae536
Mehrdad7008 <33625217+Mehrdad7008@users.noreply.github.com>
2018-10-25 17:19:03 +0330

    Add files via upload

---
+++ Floor.java
@@ -0,0 +1,30 @@
+
+public class Floor {
+	private Unit[] unit = new Unit[100000];
+	private int point;
+	private int c=3;
+	
+	public float floorPoint() {
+		return c + unitPoints() + 2 * personPoints();
+	}
+		public float personPoints() {
+			float pointOfPersons = 0 ;
+			int i;
+			for( i = 0 ; i < unit.length ; i++ ) {
+				pointOfPersons += unit[i].personPoints();
+			}
+			return pointOfPersons;
+		}
+		public float unitPoints() {
+			float pointOfUnits = 0 ;
+			int i;
+			for( i = 0 ; i < unit.length ; i++ ) {
+				pointOfUnits += unit[i].unitPoint();
+			}
+			return pointOfUnits;
+		}
+		public setFloor(int n) {
+			unit[n] = 
+		}
+}
+	
