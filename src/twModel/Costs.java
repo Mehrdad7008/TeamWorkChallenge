05771278c61cc735010fa29dc58d9bcb5390568f
alirezadaghigh99 <alireza.daghigh1999@gmail.com>
2018-10-25 16:50:00 +0330

    firsttttttasd commit

---
+++ src/twModel/Costs.java
@@ -0,0 +1,13 @@
+package twModel;
+
+public class Costs {
+    private int firstCosts = 30000;
+
+    public int getFirstCosts() {
+        return firstCosts;
+    }
+
+    public void setFirstCosts(int firstCosts) {
+        this.firstCosts = firstCosts;
+    }
+}
