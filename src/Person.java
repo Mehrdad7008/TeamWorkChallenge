31039560094fa0ea0b8845917ad331deb3686a1a
alirezadaghigh99 <alireza.daghigh1999@gmail.com>
2018-10-25 14:22:07 +0330

    firstttttt commit

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private float satisfication = 1 ;
+    private String work;
+    private int a = 1 ;
+
+    Bazaar bazaar = new Bazaar();
+
+    public float personPoint() {
+        return a * satisficationDeveloper();
+    }
+    public float satisficationDeveloper() {
+        this.satisfication *= bazaar.bazaarLvl;
+        return this.satisfication;
+    }
+    public String job() {
+        return this.work;
+    }
+
+
+}
