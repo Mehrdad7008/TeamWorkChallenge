7da6158b95d7da5133253190a81dba1ed9fb09c5
Mehrdad7008 <33625217+Mehrdad7008@users.noreply.github.com>
2018-10-25 18:10:37 +0330

    Add files via upload

--- src/twModel/Home.java
+++ src/twModel/Home.java
@@ -1,25 +1,16 @@
-package twModel;
-public class Home extends Block {
 
+public class Home extends Block {
 	private Floor[] floor = new Floor[100000];
 	private int point;
 	private float e=10;
 	private int homeId = 0 ;
 	private int cost = 3000 ;
-<<<<<<< Updated upstream
 	private int[] home = new int[15] ;
 	protected int[] homeCreate = new int[15];
     
 	public void setHomeId(int homeId) {
         this.homeId = homeId;
     }
-=======
-
-	public Home(int blockId) {
-		super(blockId);
-	}
-
->>>>>>> Stashed changes
 	public float floorPoints() {
 		float pointOfFloors = 0 ;
 		int i ;
@@ -53,24 +44,21 @@ public class Home extends Block {
 		
 	}
 	
-	public int cost() {
-		
+	public int cost(int n, int m) {
+		int cost1 = n*100 + m*300 + 700;
+		this.cost = this.cost - cost1;
+		return this.cost;
 	}
-	public void add() {
-<<<<<<< Updated upstream
+	public int add() {
 		int i ;
 	    for ( i = 0 ; i<15 ; i++)
 	        {
 	    		if (homeCreate[i]==0)
 	                break;
 	        }
-	        homeCreate[i] = 1 ;
-	        setHomeId(homeId+1);
-	    
-=======
-
-		
->>>>>>> Stashed changes
+	    homeCreate[i] = 1 ;
+	    setHomeId(homeId+1);
+	    return homeId;
 	}
 	public void upgrade() {
 		
